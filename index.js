516e40016e43d9ac6d36575d264737aec6e209fa
Samad <abdulsamodoriyomi@gmail.com>
2026-08-19 14:36:29 +0100

    Add initial project files including HTML, CSS, JS, and settings

---
+++ index.js
@@ -0,0 +1,26 @@
+//variable
+
+//mutable
+let userName = "Ganiyu";
+
+console.log(userName);
+userName = "Abdulsamod";
+console.log(userName);
+
+//immtable
+const users = 20;
+users = 10;
+console.log(typeof users);
+
+//data types
+//primitive data types
+
+//string
+let item = "CHair";
+let item = "typeof item";
+
+//number
+const myNunber = 50;
+
+console.log(`The ${items} is ${myNumber} naira`);
+alert(`The ${item} is ${myNunber} naira`);
